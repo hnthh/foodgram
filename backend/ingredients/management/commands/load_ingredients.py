7147725d919fb9bac8edb128f4fa4f7a19872929
Аня Агаренко <anyaagarenko@gmail.com>
2021-09-10 19:32:58 +0300

    Fix load_ingredients manage cmd

--- backend/ingredients/management/commands/load_ingredients.py
+++ backend/ingredients/management/commands/load_ingredients.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 from django.core.management.base import BaseCommand, CommandError
 from ingredients.models import Ingredient
@@ -9,7 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            file = open('ingredients/data/ingredients.csv')  # noqa
+            file = open('ingredients/data/ingredients.csv')  # noqa: SIM115
         except OSError:
             raise CommandError(
                 'Could not open ingredients.csv file',
@@ -18,8 +19,14 @@ class Command(BaseCommand):
         with file:
             reader = csv.reader(file)
 
-            ingredients = [
-                Ingredient(**dict(zip(('name', 'measurement_unit'), row)))
-                for row in reader
-            ]
+            ingredients = list()
+            for row in reader:
+                *parts, unit = row
+
+                name = ','.join(parts)  # handle: 'вишня, протертая с сахаром'
+                name = re.sub(r'(?<=\d),(?=\d)', '.', name)  # handle: 'молоко 3.6%'
+
+                ingredient = Ingredient(name=name, measurement_unit=unit)
+                ingredients.append(ingredient)
+
             Ingredient.objects.bulk_create(ingredients, ignore_conflicts=True)
--- backend/ingredients/tests/management/test_load_ingredients.py
+++ backend/ingredients/tests/management/test_load_ingredients.py
@@ -11,6 +11,10 @@ def test_ok(load_ingredients_command_exec):
     call_command('load_ingredients')
     assert Ingredient.objects.count() == 2191
 
+    assert Ingredient.objects.filter(name='щавель свежий', measurement_unit='веточка')
+    assert Ingredient.objects.filter(name='молоко 3.6%', measurement_unit='г')
+    assert Ingredient.objects.filter(name='вишня, протертая с сахаром', measurement_unit='г')
+
 
 def test_queries_num(django_assert_num_queries):
     with django_assert_num_queries(5):
