2d8097eebf26ce5b4034621d27a574173446c318
Аня Агаренко <anyaagarenko@gmail.com>
2021-09-11 17:19:00 +0300

    Add verbose names to Subscribe model

--- backend/users/models/subscribe.py
+++ backend/users/models/subscribe.py
@@ -1,12 +1,19 @@
 from config.models import TimestampedModel, models
 from django.db.models import F
+from django.utils.translation import gettext_lazy as _
 
 
 class Subscribe(TimestampedModel):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='followers')
-    author = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='following')
+    author = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='following',
+    )
 
     class Meta:
+        verbose_name = _('subscription')
+        verbose_name_plural = _('subscriptions')
         constraints = (
             models.CheckConstraint(name='user_not_author', check=~models.Q(user=F('author'))),
             models.UniqueConstraint(fields=('user', 'author'), name='unique_user_author'),
